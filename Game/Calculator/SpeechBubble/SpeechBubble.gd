ebcd9acc148c0b0ec7bdabb7aa9fbcc36164780c
Kyle Tiley <kylestiley@gmail.com>
2023-10-10 15:01:02 +0200

    Initial commit

---
+++ Game/Calculator/SpeechBubble/SpeechBubble.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var middle_button = $Middle
+@onready var middle_button_target = $Middle/target
+@onready var right_bubble = $Right
+@onready var label = $Label
+
+var middle_bubble_scale_default = 2
+var label_size_default = 54
+var ratio_difference = 16
+var speech_text : String
+
+func _process(_delta):
+	label.text = $TextEdit.text # for debugging
+	
+	ChangeSpeechBubbleSize()
+	right_bubble.global_position = middle_button_target.global_position
+
+func ChangeSpeechBubbleSize():
+	if label.size.x > label_size_default:
+		var new_size_diff = label.size.x - label_size_default
+		var ratio = new_size_diff / ratio_difference
+		var new_scale = middle_bubble_scale_default + ratio
+		middle_button.scale.x = new_scale
---
+++ Game/Calculator/calculator.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+enum CalculatorState{
+	IDLE,
+	SPEAKING,
+	CELEBRATING,
+}
+
+var current_state : CalculatorState
