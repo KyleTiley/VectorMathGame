1e9258c78fb0e50793aaefc950c8ba5ff43e409e
Kyle Tiley <kylestiley@gmail.com>
2023-10-11 14:34:12 +0200

    working on questions

--- Game/Graph/graph.gd
+++ Game/Graph/graph.gd
@@ -1,8 +1,22 @@
 extends Sprite2D
 
 func _ready():
+	# connects all children signals
 	for child in self.get_children():
 		child.SendPointInfo.connect(receive_point_info)
+	
+	# empties line
+	vector_line.clear_points()
 
-func receive_point_info():
-	print("Received")
+func receive_point_info(_name, _position):
+	print(str(_name) + " at " + str(_position))
+	create_vector_line()
+
+func receive_question():
+	pass
+
+# LINE CODE
+@onready var vector_line : Line2D = $"../VectorLine"
+
+func create_vector_line():
+	pass
--- Game/Graph/graph_point_area_2d.gd
+++ Game/Graph/graph_point_area_2d.gd
@@ -4,6 +4,4 @@ signal SendPointInfo
 
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
-		print(name)
-		print(position)
-		SendPointInfo.emit()
+		SendPointInfo.emit(name, position)
