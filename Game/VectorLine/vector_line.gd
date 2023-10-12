3ef1cc1d93b3e668d4e4c1bd9bc4a9d624b45bc0
Kyle Tiley <kylestiley@gmail.com>
2023-10-12 12:55:50 +0200

    arrows work

--- Game/VectorLine/vector_line.gd
+++ Game/VectorLine/vector_line.gd
@@ -7,13 +7,20 @@ func create_arrow():
 	var diff = end_point - start_point
 	print(diff)
 	
-	var vector_direction = -rad_to_deg(tanh(diff.y / diff.x)) # times negative to make easier to read
-	print(vector_direction)
-#	var first_point_to_add
-#	var second_point_to_add
-#
-#
-#	# cap off triangle
-#	self.add_point(first_point_to_add)
-#	self.add_point(second_point_to_add)
-#	self.add_point(start_point) # to finalise
+	var theta = atan2(diff.y, diff.x)
+	
+	var rad = deg_to_rad(35)
+	var arrow_length = 15
+	var x = end_point.x - arrow_length * cos(theta + rad)
+	var y = end_point.y - arrow_length * sin(theta + rad)
+	var phi = deg_to_rad(-35)
+	var x2 = end_point.x - arrow_length * cos(theta + phi)
+	var y2 = end_point.y - arrow_length * sin(theta + phi)
+	
+	var first_point_to_add = Vector2(x, y)
+	var second_point_to_add = Vector2(x2, y2)
+	
+	# cap off triangle
+	self.add_point(first_point_to_add)
+	self.add_point(second_point_to_add)
+	self.add_point(end_point) # to finalise
