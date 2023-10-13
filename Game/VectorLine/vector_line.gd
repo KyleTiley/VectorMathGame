9a9f8cb01dca371d7223fe7fed1710aa4fa46d3f
Kyle Tiley <kylestiley@gmail.com>
2023-10-13 10:55:19 +0200

    drag and drop basics works

--- Game/VectorLine/vector_line.gd
+++ Game/VectorLine/vector_line.gd
@@ -1,5 +1,6 @@
 extends Line2D
 
+@onready var line = $"."
 @onready var area = $Area2D
 @onready var collision = CollisionShape2D.new()
 
@@ -17,7 +18,7 @@ func create_arrow():
 	collision.rotation = atan(diff.y / diff.x) # angle a bit off?
 	print(collision.rotation)
 	collision.shape.extents.x = 10
-	collision.shape.extents.x = sqrt(pow(diff.x, 2) + pow(diff.y, 2))/2 # div of 2 since rez is double
+	collision.shape.extents.x = sqrt(pow(diff.x, 2) + pow(diff.y, 2))/1.8 # div of 2 since rez is double
 	
 	var theta = atan2(diff.y, diff.x)
 	var rad = deg_to_rad(35)
@@ -37,6 +38,20 @@ func create_arrow():
 	self.add_point(end_point) # to finalise
 
 
+var selected = false
+var selection_offset : Vector2
+
 func _on_area_2d_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
-		print(name)
+		selected = true
+		selection_offset = global_position - get_global_mouse_position()
+
+func _physics_process(delta):
+	print(line.global_position)
+	print(get_global_mouse_position())
+	if selected:
+		line.global_position = lerp(line.global_position, get_global_mouse_position(), 25 * delta)
+
+func _input(event):
+	if event is InputEventMouseButton and not event.pressed and event.button_index == 1:
+		selected = false
